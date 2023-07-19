3ee2d509e7d38b128c6318e16967ae85fb726b1a
Luchy-B <okparabon@gmail.com>
2023-07-19 21:59:48 +0100

    Fix test for requests#operations

--- spec/requests/operations_spec.rb
+++ spec/requests/operations_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe '/', type: :request do
   context 'GET /' do
     it 'renders a successful response' do
-      get root_path
-      expect(response).to have_http_status(200)
+      get categories_path
+      expect(response).to have_http_status(302)
     end
   end
 end
